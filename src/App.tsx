ab2a8307e378a47e33bd15490696e812989b8e01
Srikanth346 <38912054+Srikanth346@users.noreply.github.com>
2023-02-12 15:58:50 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+import { Greeting } from "./Greet";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Greeting />
+    </div>
+  );
+}
---
+++ src/Greet.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Greeting = (): JSX.Element => {
+  return <h1>{`Hello! Welcome to tRPC`}</h1>;
+};
